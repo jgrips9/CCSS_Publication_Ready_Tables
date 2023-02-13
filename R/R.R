097a82f099678bbb8ec79e151c6992ca78402949
jgrips9 <52540121+jgrips9@users.noreply.github.com>
2023-02-13 10:17:56 -0500

    Update R.R

--- R/R.R
+++ R/R.R
@@ -55,7 +55,7 @@ dev.off()
 
 
 #Export graph using package
-#install.packages("ggplot2")
+install.packages("ggplot2")
 library(ggplot2)
 ggplot(mtcars, aes(mpg, wt)) +
   geom_point(aes(colour=factor(cyl), size = qsec))
